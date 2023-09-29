4d8c9e4111f924b1f08ac907cfe34b78d54125ff
unknown <salmanfursipro@gmail.com>
2023-09-29 22:58:20 +0600

    okay

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 require("dotenv").config()
+const jwt = require('jsonwebtoken');
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
@@ -23,6 +24,25 @@ const client = new MongoClient(uri, {
    }
 });
 
+const verifyJWT = (req, res, next) => {
+   console.log('hitting varify jwt');
+   console.log(req.headers.authorization);
+   const authorization = req.headers.authorization
+   if (!authorization) {
+      return res.status(401).send({ error: true, massege: 'unothorized access' })
+   }
+   const token = authorization.split(' ')[1]
+   console.log('token inside varify zwt', token);
+   // verify a token symmetric
+   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+      if (error) {
+         return res.status(401).send({ error: true, massege: 'unothorized access' })
+      }
+      req.decoded = decoded;
+      next()
+   });
+}
+
 async function run() {
    try {
       // Connect the client to the server	(optional starting in v4.7)
@@ -31,6 +51,15 @@ async function run() {
       const serviceCollection = client.db("carDoctor").collection("services");
       const checkoutCollection = client.db("carDoctor").collection("checkout");
 
+      // JWT
+      app.post('/jwt', (req, res) => {
+         const user = req.body
+         const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5h' })
+         console.log(user, token);
+         res.send({ token })
+      });
+
+      //services routes
       app.get('/services', async (req, res) => {
          const cursor = serviceCollection.find();
          const result = await cursor.toArray();
@@ -47,7 +76,7 @@ async function run() {
          const result = await serviceCollection.findOne(query, options);
          res.json(result);
       });
-
+      // chckout routes
       app.post('/checkout', async (req, res) => {
          const order = req.body;
          console.log(order);
@@ -55,7 +84,13 @@ async function run() {
          res.send(result)
       });
 
-      app.get('/checkout', async (req, res) => {
+      app.get('/checkout', verifyJWT, async (req, res) => {
+         const decoded = req.decoded;
+
+         console.log(req.headers.authorization, "000", decoded);
+         if (decoded.email !== req.query.email) {
+            return res.status(403).send({ error: 1, massage: "forbidden access" })
+         }
          let query = {}
          if (req.query?.email) {
             query = { email: req.query.email }
@@ -72,17 +107,17 @@ async function run() {
       })
       app.patch('/checkout:id', async (req, res) => {
          const id = req.params.id
-         const updatebooking=req.body;
+         const updatebooking = req.body;
 
          const filter = { _id: new ObjectId(id) };
          const updateDoc = {
             $set: {
-              status:updatebooking.status
+               status: updatebooking.status
             },
-          };
-          console.log(updateDoc);
-          const result = await checkoutCollection.updateOne(filter, updateDoc);
-          res.send(result)
+         };
+         console.log(updateDoc);
+         const result = await checkoutCollection.updateOne(filter, updateDoc);
+         res.send(result)
       })
 
 
